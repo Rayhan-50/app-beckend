dc3c22de3271ea87955074c6a46988d21a07b6e4
Rayhan-50 <rayhanahmed.nstu@gmail.com>
2025-07-27 00:16:32 +0600

    add cors job

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 import bookRoutes from "./routes/bookRoutes.js";
 import { connectDB } from "./lib/db.js";
+import job from "./lib/cron.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
+job.start(); // Start the cron job
 app.use(express.json());
 app.use(cors());
 
